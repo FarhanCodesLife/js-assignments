eca02842b9c6f80bb367ebd38a10dfb7812d4bfc
MUHAMMADFARHANHANIF <farhansmit0318@gamil.com>
2024-05-03 04:30:00 -0700

    chapter 1

---
+++ app.js
@@ -0,0 +1,25 @@
+// chapter 01
+
+//  Qno 01
+
+// alert('password is  wrong')
+
+
+// Qno 2
+
+// alert('password is  wrong \n pleaase try leter')
+
+
+// Qno 5
+
+// alert('hello.. i am run Js through in my web console')
+
+
+
+
+
+
+// chapter 02 
+
+
+// Qno 01
