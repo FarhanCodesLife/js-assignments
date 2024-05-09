8b33dff7cd08c69ee2d58807a2f0cfb515f6948d
MUHAMMADFARHANHANIF <farhansmit0318@gamil.com>
2024-05-09 09:36:49 -0700

    if else completed

--- app.js
+++ app.js
@@ -19,7 +19,152 @@
 
 
 
-// chapter 02 
+
+
+
+
+                                 ///   if and else assignments   ///
+
+
+
 
 
 // Qno 01
+
+// var num = 3
+
+// if (num % 3 === 0) {
+//     document.write('Number is Divisible by 3')
+    
+// }
+// else{
+//     document.write('Number is not divisible by 3')
+// }
+
+
+
+
+
+// Qno 02
+
+
+// var num = 7865
+
+// if (num % 2  === 0 ) {
+//     document.write('even number')
+    
+// }
+// else{
+//     document.write('odd number')
+// }
+
+
+
+
+// Qno 03
+
+
+// var age = 18
+
+// if (age > 18) {
+//     document.write('Old enough')   
+// }
+// else{document.write('Too young')}
+
+
+
+
+
+// Qno 04
+
+// var username = prompt('enter My name')
+
+// if (username === 'farhan') {
+//     document.write('your Answer is correct')
+    
+// }
+
+
+
+// Qno 07 
+
+
+
+
+// function calculate(operator, num1, num2) {
+//     switch (operator) {
+//         case '+':
+//             return num1 + num2;
+//         case '-':
+//             return num1 - num2;
+//         case '*':
+//             return num1 * num2;
+//         case '/':
+//             if (num2 === 0) {
+//                 return "Error: Cannot divide by zero";
+//             }
+//             return num1 / num2;
+//         default:
+//             return "Error: Invalid operator";
+//     }
+// }
+
+// let operator = prompt("Enter the operator (+, -, *, /):");
+// let num1 = +prompt("Enter the first operand:");
+// let num2 = +prompt("Enter the second operand:");
+
+// let result = calculate(operator, num1, num2);
+// document.write(`result = ${result}`)
+
+
+
+
+
+
+
+// Qno 08
+
+
+var time = prompt()
+
+if (time >= 0 && time < 12 ) {
+    document.write('good Moirning')
+    
+}
+else if (time >= 12 && time < 17 ){
+    document.write('good evening')
+}
+else if(time >= 17 && time < 21 )
+{
+    document.write('good afternoon')
+
+}
+
+else if (time >= 21 && time <= 24){
+    document.write('good night')
+}
+else{
+    document.write('please enter correct hour')
+}
+
+
+
+
+
+// Qno 10 
+
+// var password = prompt('enter password')
+// var retypepassword = prompt('password Retyppe')
+
+
+// if (password === retypepassword){
+//     document.write('Password is correct')
+
+// }
+
+// else{
+//     document.write('wroung password')
+// }
+
+
+
